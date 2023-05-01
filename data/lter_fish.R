7c3e8a70bcbe6d7e1aec062124d6c8bce02bd537
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Air.local>
2023-05-01 14:12:59 -0500

    lter

--- data/lter_fish.R
+++ data/lter_fish.R
@@ -1,25 +1,21 @@
 library(tidyverse)
 
-# Data set title: North Temperate Lakes LTER: Fish Abundance 1981 - current.
-inUrl1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/7/44/6796e863c371817e3ecad736e3c2dd71" 
+# Data set title: Lake Wingra: Fish Abundance 1995 - current.
+inUrl1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/182/21/075018c60525517cf55bc11c7a9d342c" 
 infile1 <- tempfile()
 try(download.file(inUrl1,infile1,method="curl"))
-
-fish_abundance<- read_csv(infile1)
-fish_abundance_WI<-fish_abundance%>%
-  filter(lakeid == "WI")
-
-# Data set title: North Temperate Lakes LTER: Fish Species Richness 1981 - current.
-inUrl2  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/245/27/635de04f92c4f64aaa9f75594427fb61" 
-infile2 <- tempfile()
-try(download.file(inUrl2,infile2,method="curl"))
-fish_richness<- read_csv(infile2)
-fish_richness_WI<-fish_richness%>%
-  filter(lakeid == "WI")
+fish_abundance<-read_csv(infile1)
 
 # Data set title: Lake Wingra: Fish Lengths and Weights 1995 - current.
-inUrl3  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/181/22/8b668ac1a068eb9387aafd779a3f3a9c" 
+inUrl2  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/181/22/8b668ac1a068eb9387aafd779a3f3a9c" 
+infile2 <- tempfile()
+try(download.file(inUrl2,infile2,method="curl"))
+fish_length_weight<- read_csv(infile2)
+
+# Data set title: North Temperate Lakes LTER: Fish Species Richness 1981 - current.
+inUrl3  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/245/27/635de04f92c4f64aaa9f75594427fb61" 
 infile3 <- tempfile()
 try(download.file(inUrl3,infile3,method="curl"))
-fish_length_weight<- read_csv(infile3)
-
+fish_richness<- read_csv(infile3)
+fish_richness_WI<-fish_richness%>%
+  filter(lakeid == "WI")
